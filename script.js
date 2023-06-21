cdac2ceee581c1116306b18fce7f7de4b16060f8
FranAlamo <alamofrancis@gmail.com>
2023-06-21 17:41:07 -0300

    calculator ready

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
-const outputYear = document.querySelector(".output.year");
-const outputMonth = document.querySelector(".output.month");
-const outputDay = document.querySelector(".output.day");
+const outputYear = document.querySelector(".outputYear");
+const outputMonth = document.querySelector(".outputMonth");
+const outputDay = document.querySelector(".outputDay");
 const submitBtn = document.querySelector(".submit-btn");
 const inputYear = document.querySelector("#year");
 const inputMonth = document.querySelector("#month");
@@ -9,7 +9,7 @@ const inputDay = document.querySelector("#day");
 const errorYear = document.querySelector(".error-year");
 const errorMonth = document.querySelector(".error-month");
 const errorDay = document.querySelector(".error-day");
-
+submitBtn.addEventListener("click", CalculateDate);
 let isValid = false;
 inputDay.addEventListener("input", (e) => {
   if (+inputDay.value > 31) {
@@ -67,3 +67,22 @@ inputYear.addEventListener("input", (e) => {
     errorDay.textContent = "";
   }
 });
+
+function CalculateDate() {
+  if (isValid) {
+    let birthday = `${inputMonth.value}/${inputDay.value}/${inputYear.value}`;
+
+    let birthdayObj = new Date(birthday);
+    let ageDiffMill = Date.now() - birthdayObj;
+    let ageDate = new Date(ageDiffMill);
+    let ageYears = ageDate.getUTCFullYear() - 1970;
+    let ageMonth = ageDate.getUTCMonth();
+    let ageDay = ageDate.getUTCDay();
+
+    outputDay.textContent = ageDay;
+    outputMonth.textContent = ageMonth;
+    outputYear.textContent = ageYears;
+  } else {
+    alert("error");
+  }
+}
